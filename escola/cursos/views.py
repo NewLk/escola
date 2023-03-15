d40fa728f1341b513208869195f4688cd4823ec1
Newton Ribeiro <newtonafr@hotmail.com>
2023-03-15 19:13:17 -0300

    Create views for the API
    Create two API views implementing the get() method, one for each model of the app: Curso and Avaliacao.
    
    Needed for showing the API responses for the user's requests.

--- escola/cursos/views.py
+++ escola/cursos/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from . models import Curso, Avaliacao
+from . serializers import CursoSerializer, AvaliacaoSerializer
+
+class CursoAPIView(APIView):
+    """
+    API de Cursos do Newton
+    """
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+    
+class AvaliacaoAPIView(APIView):
+    """
+    API de Avaliações do Newton
+    """
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
